569b672f3453b7a363588a6c6a44f6b5feee39ab
Josue <josue.rodriguez1551@gmail.com>
2020-07-23 14:23:59 -0600

    Pong

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -7,6 +7,10 @@ namespace Kata
             if (string.IsNullOrEmpty(number))
             {
                 return 0;                
+            }else if (number.Contains(","))
+            {
+                var myArray = number.Split(",");
+                return int.Parse(myArray[0]) + int.Parse(myArray[1]);
             }
 
             return int.Parse(number);
